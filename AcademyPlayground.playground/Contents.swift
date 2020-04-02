e0f97caf0396660671dc620eae95b9852e92509d
Alanis Lima Santa Clara <alanis@br-ufpe-01.local>
2020-04-02 08:45:45 -0300

    Adicionando background e o primeiro botao com imagem

--- AcademyPlayground.playground/Contents.swift
+++ AcademyPlayground.playground/Contents.swift
@@ -11,22 +11,37 @@ class MyViewController : UIViewController {
     @IBAction func botaoTocado() {
         print("Vamos lá!")
     }
+    
 
     override func loadView() {
         
         let view = UIView()
-        view.backgroundColor = .systemBlue
+        let background = UIImageView()
+        background.image = UIImage(named: "chaoBoop")
+        background.frame = CGRect(x: 0, y: 0, width: 1440, height: 900)
+
+        //background.image = #imageLiteral(resourceName: "chaoBoop.png")
         
+        
+
         let labelBoasVindas = UILabel()
         labelBoasVindas.frame = CGRect(x: 661, y: 223, width: 761, height: 226)
         labelBoasVindas.text = "Olá, seja bem-vindo; eu sou o Boop! Vou levar você para várias aventuras!"
-        labelBoasVindas.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-        view.addSubview(labelBoasVindas)
+        labelBoasVindas.textColor = #colorLiteral(red: 0.6042942405, green: 0.604398489, blue: 0.6042804718, alpha: 1)
 
+
+        let vamosLa = UIImage (named: "vamosLa")
         botaoVamosLa.frame = CGRect(x: 973, y: 504, width: 278, height: 93)
-        botaoVamosLa.setTitle("Vamos lá!", for: .normal)
+        botaoVamosLa.setTitle("", for: .normal)
+        botaoVamosLa.setBackgroundImage(vamosLa, for: .normal)
         botaoVamosLa.setTitleColor(.black, for: .normal)
+
+        
+        view.addSubview(background)
         view.addSubview(botaoVamosLa)
+        view.addSubview(labelBoasVindas)
+        //view.sendSubviewToBack(background) //ele manda a imagem para tras, para nao interagir
+                
         
         self.view = view
 
